f901cf60209a84a987ae23af43aa285a8f2ce660
tanyajha2000 <tanyajha0108@gmail.com>
2022-03-05 23:57:26 +0530

    Initial Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { CartComponent } from './cart/cart.component';
+import { OrdersComponent } from './orders/orders.component';
+import { UserDisplayComponent } from './user-display/user-display.component';
+import { UserUpdateComponent } from './user-update/user-update.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:'/login',pathMatch:'full'},
+  {path:"login",component:LoginComponent},
+  {path:"signup",component:SignupComponent},
+  {path:"home",component:HomepageComponent},
+  {path:"cart",component:CartComponent},
+  {path:"orders",component:OrdersComponent},
+  {path: 'user', component: UserDisplayComponent},
+  {path: 'update-user/:id', component: UserUpdateComponent},
+  {path: 'dashboard', component:AdminDashboardComponent},
+  {path:"**",component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,18 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { UserNavbarComponent } from './user-navbar/user-navbar.component';
+import { CartComponent } from './cart/cart.component';
+import { OrdersComponent } from './orders/orders.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
+import { UserDisplayComponent } from './user-display/user-display.component';
+import { UserUpdateComponent } from './user-update/user-update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-    
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    NotFoundComponent,
+    HomepageComponent,
+    UserNavbarComponent,
+    CartComponent,
+    OrdersComponent,
+    AdminDashboardComponent,
+    UserDisplayComponent,
+    UserUpdateComponent 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    Ng2SearchPipeModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
