8d90e8ee1fa46e081cd1f367e0b15ed0d62ac025
Smritica Kumari <75357586+Smritica@users.noreply.github.com>
2022-03-13 17:45:38 +0530

    Update CartService.java

--- springapp/src/main/java/com/examly/springapp/service/CartService.java
+++ springapp/src/main/java/com/examly/springapp/service/CartService.java
@@ -1,5 +1,44 @@
 package com.examly.springapp.service;
 
-public class CartService {
-    
-}
+import java.util.List;
+
+import com.examly.springapp.repository.CartRepository;
+import com.examly.springapp.model.Cart;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+//import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CartService  {
+
+    private final  CartRepository cRepository;
+
+    @Autowired
+    public CartService(CartRepository cRepository){
+        this.cRepository = cRepository;
+    }
+
+    public List<Cart>getAllCarts(){
+        return cRepository.findAll();
+    }
+
+    public void addToCart(Cart cart) {
+		 cRepository.save(cart);
+	}
+
+    public void deleteCartById(Long cartId) {
+        boolean exists = cRepository.existsById(cartId);
+        if (!exists){
+            throw new IllegalStateException(" cart with id "+ cartId + " does not exists");
+        }
+        cRepository.deleteById(cartId);
+    }
+    public void updateCart(Cart cart){
+         cRepository.save(cart);
+    }
+
+
+    }
