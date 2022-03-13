706c4bd5b1ddf021f4f133438ed736a5cbd199ab
Mansi kumari <49369305+1620mansi@users.noreply.github.com>
2022-03-13 16:03:29 +0530

    Update app.module.ts

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
-    LoginComponent,
-    NotFoundComponent
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
+    
     HttpClientModule
   ],
   providers: [],
