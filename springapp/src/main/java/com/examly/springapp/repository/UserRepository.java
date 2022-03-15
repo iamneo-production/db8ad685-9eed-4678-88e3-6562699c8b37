0b64c37c7d97c1d6d5c1d8da2a9fea59239344d0
tanyajha2000 <50415127+tanyajha2000@users.noreply.github.com>
2022-03-15 21:22:36 +0530

    Update UserRepository.java

--- springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/UserRepository.java
@@ -1,10 +1,10 @@
-package com.example.demo.repository;
+package com.examly.springapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.User;
+import com.examly.springapp.model.User;
 
 import org.springframework.data.repository.query.Param;
 @Repository
