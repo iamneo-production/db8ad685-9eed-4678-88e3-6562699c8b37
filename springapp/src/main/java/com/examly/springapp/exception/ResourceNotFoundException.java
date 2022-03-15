fc651a29570464913d83cf9cef083b179d3f7e22
tanyajha2000 <50415127+tanyajha2000@users.noreply.github.com>
2022-03-15 21:18:32 +0530

    Update ResourceNotFoundException.java

--- springapp/src/main/java/com/examly/springapp/exception/ResourceNotFoundException.java
+++ springapp/src/main/java/com/examly/springapp/exception/ResourceNotFoundException.java
@@ -1,5 +1,5 @@
 
-package com.example.demo.exception;
+package com.examly.springapp.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value=HttpStatus.NOT_FOUND)
