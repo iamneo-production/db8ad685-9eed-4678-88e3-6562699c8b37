30a91e52e75d412ac8ffb8a4360dbe813e6901f3
tanyajha2000 <50415127+tanyajha2000@users.noreply.github.com>
2022-03-15 21:22:06 +0530

    Update ProductRepository.java

--- springapp/src/main/java/com/examly/springapp/repository/ProductRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/ProductRepository.java
@@ -1,9 +1,9 @@
-package com.example.demo.repository;
+package com.examly.springapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.Product;
+import com.examly.springapp.model.Product;
 
 	@Repository
 	public interface ProductRepository extends JpaRepository<Product, Long>{
