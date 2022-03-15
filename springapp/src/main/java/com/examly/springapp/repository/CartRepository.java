da24a18a421ea21dbe3f3eb96f6e5a9104ca2219
tanyajha2000 <50415127+tanyajha2000@users.noreply.github.com>
2022-03-15 21:21:30 +0530

    Update CartRepository.java

--- springapp/src/main/java/com/examly/springapp/repository/CartRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/CartRepository.java
@@ -1,10 +1,10 @@
 
-package com.example.demo.repository;
+package com.examly.springapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.model.Cart;
+import com.examly.springapp.model.Cart;
 
 
 @Repository
