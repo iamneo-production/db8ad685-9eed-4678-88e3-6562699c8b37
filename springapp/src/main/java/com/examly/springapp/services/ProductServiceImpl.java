98af98b0e18664e4134b79a4afbec3ee921ff904
tanyajha2000 <50415127+tanyajha2000@users.noreply.github.com>
2022-03-16 20:29:35 +0530

    Update ProductServiceImpl.java

--- springapp/src/main/java/com/examly/springapp/services/ProductServiceImpl.java
+++ springapp/src/main/java/com/examly/springapp/services/ProductServiceImpl.java
@@ -1,13 +1,13 @@
-package com.example.demo.services;
+package com.examly.springapp.services;
 
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.model.Product;
-import com.example.demo.repository.ProductRepository;
+import com.examly.springapp.exception.ResourceNotFoundException;
+import com.examly.springapp.model.Product;
+import com.examly.springapp.repository.ProductRepository;
 
 @Service
 public class ProductServiceImpl implements ProductService {
