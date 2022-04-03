c0690cf979462cbf22797b6e69f78bde1cdf1790
tanyajha2000 <50415127+tanyajha2000@users.noreply.github.com>
2022-04-03 17:38:20 +0530

    Add files via upload

---
+++ springapp/src/main/java/com/examly/springapp/controller/CheckoutController.java
@@ -0,0 +1,41 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.DTO.Purchase;
+import com.example.demo.DTO.PurchaseResponse;
+import com.example.demo.services.CheckoutService;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/v1/")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+
+}
+
+
+
+
+
+
+
+
+
